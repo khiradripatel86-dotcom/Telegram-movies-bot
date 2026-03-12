7bb3cc9d5f3429dbcb074620169cefc9dd5c6f5f
khiradripatel86-dotcom <khiradripatel86@gmail.com>
2026-03-12 22:13:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,40 @@
-import os
 from pyrogram import Client, filters
-from database import add_movie,get_movie
 
-BOT_TOKEN=os.getenv("BOT_TOKEN")
-API_ID=int(os.getenv("35790707"))
-API_HASH=os.getenv("44753bcac8911c81028f009377368330")
+API_ID = 35790707
+API_HASH = "44753bcac8911c81028f009377368330"
+BOT_TOKEN = "8779345278:AAGUeRRmEx0C2MP2q4xDmNQUVlcyh_GRRR4"
+
+CHANNEL_ID = -1003865059298
 
 bot = Client(
     "moviebot",
-    bot_token=BOT_TOKEN,
     api_id=API_ID,
-    api_hash=API_HASH
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
 )
 
+movies = {}
+
 @bot.on_message(filters.command("start"))
-async def start(client,message):
-    await message.reply("🎬 Movie Bot Ready\nUse /movie movie_name")
+async def start(client, message):
+    await message.reply("🎬 Movie Bot Ready\nUse /movie name")
 
 @bot.on_message(filters.command("movie"))
-async def movie(client,message):
+async def movie(client, message):
 
-    name = message.text.split(" ",1)[1]
+    name = message.text.split(" ",1)[1].lower()
 
-    data = get_movie(name)
-
-    if data:
-        await message.reply_document(data[0])
+    if name in movies:
+        await message.reply_document(movies[name])
     else:
         await message.reply("❌ Movie not found")
 
-# admin command to add movie
-@bot.on_message(filters.document)
-async def save_movie(client,message):
+@bot.on_message(filters.chat(CHANNEL_ID) & filters.document)
+async def save_movie(client, message):
 
-    name = message.document.file_name
-
+    name = message.document.file_name.lower()
     file_id = message.document.file_id
 
-    add_movie(name,file_id)
-
-    await message.reply("✅ Movie saved in database")
+    movies[name] = file_id
 
 bot.run()
